98e4d2bec4a4facd847c4e46efdb88bb50fdc24a
seyma-altiparmak <102421136+seyma-altiparmak@users.noreply.github.com>
2022-11-30 02:37:10 +0300

    UserClass
    -user model is importing

---
+++ eCommerceWebsite/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace eCommerceWebsite.Models
+{
+    public class User
+    {
+        [Key]
+        public int ActorId { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+    }
+}
