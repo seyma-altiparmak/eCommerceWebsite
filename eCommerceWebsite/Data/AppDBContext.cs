9a0fc896eee2ac934ae0362b2e39dc4558116c0d
seyma-altiparmak <102421136+seyma-altiparmak@users.noreply.github.com>
2022-11-30 03:18:45 +0300

    Release 1.0.3
    DB Problems

--- eCommerceWebsite/Data/AppDBContext.cs
+++ eCommerceWebsite/Data/AppDBContext.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using eCommerceWebsite.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 namespace eCommerceWebsite.Data
 {
-    public class AppDBContext:DbContext
+    public class AppDBContext : DbContext
     {
-        public AppDBContext(DbContextOptions<AppDBContext> options): base(options)
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Company_User>().HasKey(e => new
+            {
+                e.UserId,
+                e.CompanyId
+            } );
+
+            modelBuilder.Entity<Company_User>().HasOne(C => C.company).WithMany(e => e.Companies_Users).HasForeignKey(C => C.UserId);
+            //13-11
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
--- eCommerceWebsite/Models/Product.cs
+++ eCommerceWebsite/Models/Product.cs
@@ -1,5 +1,6 @@
-﻿using eCommerceWebsite.Data;
+﻿using eCommerceWebsite.Data.Enums;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eCommerceWebsite.Models
 {
@@ -9,6 +10,17 @@ namespace eCommerceWebsite.Models
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public string ProductDescription { get; set; }
+        public DateTime Sale_StartDate { get; set; }
+        public DateTime Sale_EndDate { get; set; }
         public ProductCategory ProductCategory { get; set; }
+
+        //Relationship
+        public List<Company_User> Companies_Users { get; set; }
+
+        //User
+        [ForeignKey("UserId")]
+        public int UserId { get; set; }
+        public User user { get; set; }
+
     }
 }
